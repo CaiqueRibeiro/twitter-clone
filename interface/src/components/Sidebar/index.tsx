cf9848e67096229f089c90091cd3c70ab813ece4
Caique <ribeiro.caique95@gmail.com>
2023-07-08 18:24:04 -0300

    chore: [interface] add sidebar profile button

--- interface/src/components/Sidebar/index.tsx
+++ interface/src/components/Sidebar/index.tsx
@@ -1,9 +1,10 @@
 import { Twitter } from 'lucide-react'
 import Menu from './Menu'
+import ProfileButton from './ProfileButton'
 
 export default function Sidebar() {
   return (
-    <div className='w-72 flex flex-col justify-between pl-1'>
+    <div className='w-72 flex flex-col justify-between pl-1 pr-2'>
       <div className='flex flex-col gap-1 items-start'>
         <div className='h-14 flex items-center justify-center rounded-full w-14 transition ease-in-out hover:bg-zinc-900 duration-200'>
           <Twitter fill="white" color='none' size={30} />
@@ -13,8 +14,8 @@ export default function Sidebar() {
           <span className='text-white text-lg'>Tweet</span>
         </div>
       </div>
-
-      <div className='h-14'> Perfil</div>
+      
+      <ProfileButton />
     </div>
   )
 }
